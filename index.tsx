b85554eece1c464ee3b7af34bc2ba45261255787
WhyK <windchimeyk@gmail.com>
2024-07-23 00:50:48 +0900

    refactor: update top page layout

--- index.tsx
+++ index.tsx
@@ -25,13 +25,18 @@ const jsx = () => (
   <html>
     <head>
       <title>WhyK Slides</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <link rel="stylesheet" href="https://fonts.xz.style/serve/inter.css" />
       <link
         rel="stylesheet"
         href="https://cdn.jsdelivr.net/npm/@exampledev/new.css@1.1.2/new.min.css"
       />
     </head>
-    <body>
+    <body style={{
+      minHeight: '93vh', // new.cssのpadding分ザックリ抜いた
+      display: 'grid',
+      gridTemplateRows: 'auto 1fr auto',
+    }}>
       <header>WhyK Slides</header>
       <main>
         <p>
