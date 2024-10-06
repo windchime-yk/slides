6ab2deca411223f82ae089ac95f9d0255ad27f6b
WhyK <windchimeyk@gmail.com>
2024-10-06 23:24:08 +0900

    [Slide] WinterCG Runtime Keysについて (#12)
    * chore(scripts): update dev.ts
    
    * docs(slides): add wdc2024-miror
    
    * docs(slides/wdc2024-miror): update image link

--- scripts/dev.ts
+++ scripts/dev.ts
@@ -1,13 +1,11 @@
 import $ from "jsr:@david/dax@0.40.0";
-// import { getSlidePaths } from "./lib.ts";
+import { getSlidePaths } from "./lib.ts";
 
-// const slidePaths = await getSlidePaths();
-// const selectedPathPosition = await $.select({
-//   message: "どのスライドを起動する？",
-//   options: slidePaths,
-// });
+const slidePaths = await getSlidePaths();
+const selectedPathPosition = await $.select({
+  message: "どのスライドを起動する？",
+  options: slidePaths,
+});
 
-// TODO: $.selectが2つ以上の選択肢がないと起動しないため、後ほどコメントアウトを外す
-// $.cd(`${Deno.cwd()}/slides/${slidePaths[selectedPathPosition]}`)
-$.cd(`${Deno.cwd()}/slides/introduce-bid`);
+$.cd(`${Deno.cwd()}/slides/${slidePaths[selectedPathPosition]}`)
 await $`pnpm dev`;
